fea020204a114ca5add0e4dc5bf16e0d5d3596ae
Yoonsung <lvev9925@naver.com>
2015-07-08 19:13:50 +0900

    Add inmemory based Spring-Security Setting

---
+++ src/main/java/config/GatewayApplication.java
@@ -0,0 +1,20 @@
+package config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+//@SpringBootApplication
+@Configuration
+@Import({
+        SecurityConfig.class,
+        WebConfig.class
+})
+@EnableAutoConfiguration
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ src/main/java/config/WebConfig.java
@@ -0,0 +1,12 @@
+package config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by yoon on 15. 7. 8..
+ */
+@Configuration
+@ComponentScan({"web"})
+public class WebConfig {
+}
