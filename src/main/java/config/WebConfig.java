ce7ad79cca4f3085664e0caaa9c7daced8841a0c
Yoonsung <lvev9925@naver.com>
2015-07-17 19:32:28 +0900

    Add Login Interceptor

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -2,11 +2,25 @@ package config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import web.interceptor.AuthenticationInterceptor;
 
 /**
  * Created by yoon on 15. 7. 8..
  */
 @Configuration
 @ComponentScan({"web"})
-public class WebConfig gi{
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    public static final String COOKIE_NAME = "MSA_COOKIE";
+    public static final String TEST_COOKIE_VALUE = "abcdefg";
+
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthenticationInterceptor())
+            .addPathPatterns("/**")
+            .excludePathPatterns("/users");
+
+    }
 }
